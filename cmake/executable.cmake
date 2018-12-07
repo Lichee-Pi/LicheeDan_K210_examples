bdea16d9db80651ea2b9de16510f343a3f543037
xel <xel1995@163.com>
2018-12-07 10:52:00 +0800

    git clone from https://github.com/kendryte/kendryte-standalone-sdk.git @commit id d13b38098a3598b0018c38590387e43953efd140

---
+++ cmake/executable.cmake
@@ -0,0 +1,36 @@
+if (NOT BUILDING_SDK)
+    add_library(kendryte STATIC IMPORTED)
+    set_property(TARGET kendryte PROPERTY IMPORTED_LOCATION ${SDK_ROOT}/libkendryte.a)
+    include_directories(${SDK_ROOT}/include/)
+endif ()
+
+removeDuplicateSubstring(${CMAKE_C_FLAGS} CMAKE_C_FLAGS)
+removeDuplicateSubstring(${CMAKE_CXX_FLAGS} CMAKE_CXX_FLAGS)
+
+message("SOURCE_FILES=${SOURCE_FILES}")
+add_executable(${PROJECT_NAME} ${SOURCE_FILES})
+
+
+set_target_properties(${PROJECT_NAME} PROPERTIES LINKER_LANGUAGE C)
+
+target_link_libraries(${PROJECT_NAME}
+        -Wl,--start-group
+        gcc m c
+        -Wl,--whole-archive
+        kendryte
+        -Wl,--no-whole-archive
+        -Wl,--end-group
+        )
+
+IF(SUFFIX)
+    SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES SUFFIX ${SUFFIX})
+ENDIF()
+
+# Build target
+add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+        COMMAND ${CMAKE_OBJCOPY} --output-format=binary ${CMAKE_BINARY_DIR}/${PROJECT_NAME}${SUFFIX} ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.bin
+        DEPENDS ${PROJECT_NAME}
+        COMMENT "Generating .bin file ...")
+
+# show information
+include(${CMAKE_CURRENT_LIST_DIR}/dump-config.cmake)
